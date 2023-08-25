25a28db45be7b08b47ddc305d6b1e6b637e7bd6e
MoaeedRandhawa <241548737@formanite.fccollege.edu.pk>
2023-08-26 00:22:37 +0500

    NewsApp with class based components

---
+++ src/components/NewsItem.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export default class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+
+          <img
+            className="card-img-top"
+            src={
+              !imageUrl
+                ? "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/11787/production/_124395517_bbcbreakingnewsgraphic.jpg"
+                : imageUrl
+            }
+            alt="..."
+            
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small>
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
